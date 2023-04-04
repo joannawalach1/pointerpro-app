3dc8091d3df37098d285eb64a1578934a49a9a0c
Joanna Wałach <joanna.walach@op.pl>
2023-04-04 23:15:37 +0200

    adding dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,30 @@
 import React from "react";
+import AllActivities from "../components/AllActivities";
 import DiagramField from "../components/DiagramField";
+import LatestNews from "../components/LatestNews";
 import Navbar from "../components/Navbar";
+import RecentQuest from "../components/RecentQuest";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
-    <Navbar />
-    <DiagramField />
+      <Navbar />
+      <div className="dashboard__left">
+        <DiagramField />
+        <div className="dashboard__bottom">
+          <div className="dashboard__recent">
+            <RecentQuest />
+          </div>
+          <div className="dashboard__allact">
+            <AllActivities />
+          </div>
+        </div>
+      </div>
+      <div className="dashboard__right">
+        <LatestNews />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
